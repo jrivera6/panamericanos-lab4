3efcdcd0c0f8f7437716f4dc0c9a93ad79a176a7
Jhonny Rivera <jhonny.riverap6@gmail.com>
2019-03-20 15:56:32 -0500

    Prueba login corregida 2

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     $user = $_POST["txt_usuarios"];
     $pass = $_POST["txt_contraseña"];
 
-    $result =  mysqli_query("SELECT * FROM usuarios WHERE username = '$user'");
+    $result =  mysqli_query("SELECT * FROM usuarios WHERE username = $user");
 
     $row = mysqli_fetch_array($result);
 
