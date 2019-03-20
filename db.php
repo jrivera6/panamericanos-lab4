28091f843a61494d625815ed6999d051345675ab
Jhonny Rivera <jhonny.riverap6@gmail.com>
2019-03-20 15:47:28 -0500

    Prueba login2

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-    $server="";
-    $user= "";
-    $password= "";
-    $database= "";
+    $server="us-cdbr-iron-east-03.cleardb.net";
+    $user= "bdd22bb55b1bca";
+    $password= "a5dd608f";
+    $database= "usuarios";
 
 
 
