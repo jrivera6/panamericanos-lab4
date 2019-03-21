2a32c1542ec9cb0cf6cdbeeff30a3ff398330f25
Jhonny Rivera <jhonny.riverap6@gmail.com>
2019-03-20 22:39:43 -0500

    TerminandoCRUD

--- update.php
+++ update.php
@@ -21,8 +21,8 @@ $db = mysqli_connect($server,$user,$password,$database);
         $hist = $_POST["txt_newhistoria"];
 
 
-        $insert = "UPDATE deportes SET nombre = '$txt_newdeporte', descripcion = '$txt_newdescripcion,
-        historia ='$txt_newhistoria' WHERE id='$id'";
+        $insert = "UPDATE deportes SET nombre = '$nom_deporte', descripcion = '$desc',
+        historia ='$hist' WHERE id='$id'";
 
 
         if(mysqli_query($db,$insert)) {
