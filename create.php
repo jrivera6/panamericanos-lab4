1f365e06abca38a58e25be3232c3ee9d3fd859bb
Jhonny Rivera <jhonny.riverap6@gmail.com>
2019-03-20 20:24:08 -0500

    Lista de datos act4

---
+++ create.php
@@ -0,0 +1,24 @@
+<?php
+
+    include "db.php";
+
+    $db = mysqli_connect($server,$user,$password,$database);
+
+    $nom_deporte  = $_POST["txt_newdeporte"];
+    $desc = $_POST["txt_newdescripcion"];
+    $hist = $_POST["txt_newhistoria"];
+
+    $insert = "INSERT INTO deportes (nombre, descripcion, historia)
+    VALUES ('$nom_deporte', '$desc', '$hist')";
+
+    $result = $db->query($insert);
+
+    if($result === TRUE) {
+
+        
+    } else {
+        
+    }   
+
+
+?>
